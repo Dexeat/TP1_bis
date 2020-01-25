4a2de6bce2edf00ada6bc0f00ef0ebb41f70e4bd
Dexeat <Dexeat@users.noreply.github.com>
2020-01-25 17:57:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,17 @@ struct Monstre
     int attsort;
 };
 
+struct Joueur
+{
+    int pv;
+    int att;
+    int PM;
+    int attsort;
+};
+
+
 typedef struct Monstre monstre;
+typedef struct Joueur joueur;
 
 //aléa
 int genRandoms(int lower, int upper, int count)
@@ -34,8 +44,14 @@ void clrscr()
     system("@cls||clear");
 }
 
+int tourJoueur(int PM)
+
+
 
 int main(void){
+
+    int game;
+    int choix;
 
     //variable des monstre
     monstre demon_inferieur = {50, 4, 6};
@@ -48,10 +64,20 @@ int main(void){
 
     monstre combat[3];
 
+    //PlayerStats
+    joueur player = {50,5,20,15};
+
     //génère les monstre
     for (int i = 0; i < 3; i++)
     {
         combat[i] = geneCombat(tableMonstre, 0, 4, 1);
         printf("PV:%d ATT:%d ATTS:%d\n", combat[i].pv, combat[i].att, combat[i].attsort);
     }
+
+    while(game){
+
+        choix = tourJoueur(player.PM);
+
+    }
+
 }
